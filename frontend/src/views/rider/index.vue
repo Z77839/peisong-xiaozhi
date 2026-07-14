16f2cb684c6dab0a59fc32ad164afe9b430c84e4
007 <3509086895@qq.com>
2026-07-14 15:09:23 +0800

    fix: 修 for loop + style 块里的孤儿 import

--- frontend/src/views/rider/index.vue
+++ frontend/src/views/rider/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import KnowledgeHints from '@/components/KnowledgeHints.vue'
 import { ref, onMounted, shallowRef, computed, watch } from 'vue'
 import * as echarts from 'echarts'
 import ChartCard from '@/components/ChartCard.vue'
@@ -331,7 +332,6 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 @use "@/assets/styles/variables.scss" as *;
-import KnowledgeHints from '@/components/KnowledgeHints.vue'
 
 .page-container {
   padding: 20px;
